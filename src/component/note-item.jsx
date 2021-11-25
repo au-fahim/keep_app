b4a19b1d130384ecaef7ca67a119ca0b7e9fec2a
Ashraf_uddin <dev.fahim2021@gmail.com>
2021-11-25 14:18:48 +0600

    Created Basic Structure for Keeper App

---
+++ src/component/note-item.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+function NoteItem() {
+  return (
+    <div className="note-item">
+      <h3>Note Title</h3>
+      <p>This is the sample item content</p>
+    </div>
+  );
+}
+
+export default NoteItem;
