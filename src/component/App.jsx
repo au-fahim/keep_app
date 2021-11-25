57e25e186ad0fbe8b8abcd94e472b360f6f2cf99
Ashraf_uddin <dev.fahim2021@gmail.com>
2021-11-25 11:03:21 +0600

    Starting React keep note project

---
+++ src/component/App.jsx
@@ -0,0 +1,18 @@
+import React, {Component} from "react"
+import Header from "./header"
+import Footer from "./footer"
+import Note from "./note"
+
+// NOTE: There are added all external components.
+
+class App extends Component {
+
+  render() {
+    return (
+      <Header />
+      <Footer />
+    )
+  }
+}
+
+export default App;
---
+++ src/component/header.jsx
@@ -0,0 +1,11 @@
+import React from "react"
+
+
+      class Header extends Component {
+        render(){
+          return(
+            <header>
+            </header>
+          )
+        }
+      }
